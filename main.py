41c6bcad06b0e4cc67b658866188fb2311183082
James <jamestephen@shaw.ca>
2026-01-26 21:26:06 -0700

    Added SSH key generation function

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import customtkinter
 import os
 import pygit2
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -42,7 +45,39 @@ def create_folder():
         os.makedirs(dolphin_sync_credentials)
     
 
+def create_ssh_keys():
+    # Generate and serialize the private key
+    private_key = ed25519.Ed25519PrivateKey.generate()
+    private_bytes = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.OpenSSH,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    # Generate and serialize the public key
+    public_key = private_key.public_key()
+    public_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.OpenSSH,
+        format=serialization.PublicFormat.OpenSSH
+    )
+
+    # Add to "./Dolphin iOS Sync/Credentials"
+    app_data_path = os.path.join(os.getenv('APPDATA'), 'DolphinSync')
+    dolphin_sync_credentials = os.path.join(app_data_path, 'Credentials')
+    private_key_path = os.path.join(dolphin_sync_credentials, "id_ed25519")
+    public_key_path = os.path.join(dolphin_sync_credentials, "id_ed25519.pub")
+
+    with open(private_key_path, "wb") as f:
+        f.write(private_bytes)
+    # Ensure only the user can read and write
+    os.chmod(private_key_path, 0o600)
+
+    with open(public_key_path, "wb") as f:
+        f.write(public_bytes)
+
 create_folder()
+
+create_ssh_keys()
 
 app = App()
 app.mainloop()
